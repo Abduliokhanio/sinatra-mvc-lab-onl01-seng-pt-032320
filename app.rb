0cdc68379aa54d6dea0c4f9f4ccacf96cf2a4171
Abdul Khan <akhan17@outlook.com>
2020-05-24 20:16:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,8 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    @putinay_input = PigLatinizer.new.to_pig_latin(params[:user_phrase])
-    erb :piglatinized
+    @text = params[:user_phrase]
+    @piglatinizer = PigLatinizer.new
+    erb :piglatinize
   end
 end
