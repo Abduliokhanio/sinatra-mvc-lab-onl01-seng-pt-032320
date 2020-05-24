406a74ac004a440bff8b4961828c2cdc06a3fe59
Abdul Khan <akhan17@outlook.com>
2020-05-24 20:08:55 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,68 +1,18 @@
 class PigLatinizer
+  attr_accessor :piglatinized_phrase
 
-  def initialize(text)
-    @text = text.downcase
+def piglatinize(word) #pig latinizes a single word
+  if word.downcase.index(/[aeiou]/) == 0
+    word + "way"
+  else
+    vowel_index = word.index(/[aeiou]/)
+    front_end = word.slice!(0..vowel_index-1)
+    word + front_end +"ay"
   end
-
-  def translate
-    str = @text
-
-    # Vowels to consider
-    vowels = ["a", "e", "i", "o", "u"]
-
-    # Special cases to consider
-    two_letter_consonants = ["ch", "sh", "qu", "th", "br"]
-    three_letter_consonants = ["thr", "sch", "squ"]
-
-    # Seperate each word from the phrase given
-    words = str.split(" ")
-
-    #Location for processed words
-    result = [];
-
-
-
-    words.each do |word|
-        # Words that start with a vowels
-        if vowels.include? word[0]
-            result.push word << 'ay'
-
-        # Words that start with a consonant
-        else
-            # Check for special consonants
-            if three_letter_consonants.include? word[0] + word[1] + word[2]
-                # Slice off first three letters
-                first_three_letters = word.slice!(0,3)
-
-                # Add letters to end of word with 'ay'
-                result.push word << first_three_letters << 'ay'
-
-            elsif  two_letter_consonants.include? word[0] + word[1]
-                # Slice off first two letters
-                first_two_letters = word.slice!(0,2)
-
-                # Add the letters to end of word with 'ay'
-                result.push word << first_two_letters << 'ay'
-
-            else
-                # Slice off first letter...
-                first_letter = word.slice!(0)
-
-                # Add first letter to end of word with 'ay'
-                result.push word << first_letter << 'ay'
-            end #End of special consonant check
-
-        end #End of vowel check
-
-
-
-    end #End of words.each
-
-
-
-    #Present the processed words as a single string
-    return result.join(" ")
-
-end #End of translate function
-
 end
+
+def to_pig_latin(phrase)
+  word_array = phrase.split(" ")
+  @piglatinized_phrase = word_array.collect {|word| piglatinize(word)}.join(" ")
+end
+end #class end
